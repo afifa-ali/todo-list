71a4479deed7d9330a427bdb7ac7be9b5c676e74
afifa-ali <afifaali973@gmail.com>
2025-08-10 09:40:02 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+    assetModuleFilename: 'assets/[name][ext]' // Optional: keeps original names
+  },
+  // mode: 'development', ← leave this out if you're setting it via CLI
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true
+  },
+  devtool: 'source-map'
+};
+
+
+/* npm install --save-dev webpack webpack-cli webpack-dev-server \
+html-webpack-plugin html-loader css-loader style-loader \
+@babel/core @babel/preset-env babel-loader */
+
